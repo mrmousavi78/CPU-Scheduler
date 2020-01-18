135bbd2e287b61b36f7dd5bc36d5015eb9a7eaf6
SMRMousavi <mrmousavi78@gmail.com>
2020-01-18 14:15:25 +0330

    -add some getter functions to process class -update FCFS method in Scheduler class -add update_ready_queue method to Scheduler class

--- enums.py
+++ enums.py
@@ -7,3 +7,10 @@ class State(Enum):
     CPU = 2
     IO = 3
     TERMINATED = 4
+
+
+class Algorithm(Enum):
+    FCFS = 0
+    RR = 1
+    SPN = 2
+    SRT = 3
--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,6 @@
+from enums import Algorithm, State
+
+
 class Scheduler:
     """
     A machine that simulate the execution of processes in different algorithms. :)
@@ -5,9 +8,9 @@ class Scheduler:
 
     def __init__(self):
         self.__state = None
-        self.__current_algorithm = None
+        self.__algorithm = None
         self.__processes = []
-        self.__running_process = 0
+        self.__running_process = None
         self.__ready_queue = []  # TODO: find a implementation for queue
         self.__done_list = []
         self.__timer = 0
@@ -15,10 +18,22 @@ class Scheduler:
     def get_processes(self, processes):
         self.__processes = processes
 
+    def update_ready_queue(self):
+        for proc in self.__processes:
+            if proc.get_next_arrival_time() == self.__timer:
+                self.__ready_queue.append(proc)
+        if self.__algorithm == Algorithm.SPN or self.__algorithm == Algorithm.SRT:
+            self.__ready_queue.sort(key=lambda x: x.get_current_burst_time())
 
     def FCFS(self):
-        pass
+        while len(self.__done_list) != len(self.__processes):
+            if self.__ready_queue:
+                self.update_ready_queue()
+                self.__running_process = self.__ready_queue.pop(0)
+                self.__running_process.minus_burst_time()
 
+
+        pass
 
     def run(self):
         pass
