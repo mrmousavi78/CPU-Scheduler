bb6e0f42130ee15e3d1cc3d3ce3b2ae0b6845178
vahidgh <vahidgh.vgh@gmail.com>
2020-01-20 01:42:04 +0330

    - add test for scheduler in main

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from scheduler import Scheduler
 from process import Process
 from enums import Algorithm
+import copy
 
 processes = []
 for i in range(3):
-    processes.append(Process(i + 1, i, [10], [2]))
+    processes.append(Process(i + 1, i, [12, 6], [8]))
 
+processes_fcfs = copy.deepcopy(processes)
+processes_rr = copy.deepcopy(processes)
+processes_spn = copy.deepcopy(processes)
+processes_srt = copy.deepcopy(processes)
 scheduler_fcfs = Scheduler()
 scheduler_rr = Scheduler()
 scheduler_spn = Scheduler()
 scheduler_srt = Scheduler()
-scheduler_fcfs.processes = processes
-scheduler_rr.processes = processes
-scheduler_spn.processes = processes
-scheduler_srt.processes = processes
+scheduler_fcfs.processes = processes_fcfs
+scheduler_rr.processes = processes_rr
+scheduler_spn.processes = processes_spn
+scheduler_srt.processes = processes_srt
 scheduler_fcfs.run(Algorithm.FCFS)
 scheduler_rr.run(Algorithm.RR)
 scheduler_spn.run(Algorithm.SPN)
